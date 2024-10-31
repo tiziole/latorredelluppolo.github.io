46b56a61f0850022d2fc285c41e225a9fe163e54
tiziole <tizianolettieri@gmail.com>
2024-10-31 01:56:58 +0100

    script refresh

---
+++ refreshtop.js
@@ -0,0 +1,6 @@
+// REFRESH ON TOP
+document.addEventListener("DOMContentLoaded", function () {
+    // Ensure the page refreshes at the top
+    history.scrollRestoration = "manual";
+    window.scrollTo(0, 0);
+});
