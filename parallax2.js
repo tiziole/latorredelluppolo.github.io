d159a34ded04a1b191a8cdf3c2529c2cea7425be
tiziole <tizianolettieri@gmail.com>
2025-01-12 23:18:24 +0100

    new grid8

--- parallax2.js
+++ parallax2.js
@@ -1,7 +1,7 @@
 function setParallaxEffect() {
         var cards = document.querySelectorAll('.card');
         var windowHeight = window.innerHeight;
-        var startTop = 60; // Valore iniziale di top
+        var startTop = 30; // Valore iniziale di top
         var endTop = 0; // Valore finale di top
 
         cards.forEach(function(card) {
