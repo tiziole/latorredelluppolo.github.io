8346a9a020a8aaf03c142e6091262551c747d8a2
tiziole <tizianolettieri@gmail.com>
2025-01-14 23:44:03 +0100

    update parallax1.9

--- parallax2.js
+++ parallax2.js
@@ -1,8 +1,8 @@
 function setParallaxEffect() {
         var cards = document.querySelectorAll('.card');
         var windowHeight = window.innerHeight;
-        var startTop = 15; // Valore iniziale di top
-        var endTop = -15; // Valore finale di top
+        var startTop = 35; // Valore iniziale di top
+        var endTop = -5; // Valore finale di top
 
         cards.forEach(function(card) {
             var image = card.querySelector('.card-image');
