c5de41617f3f6c2a274c3002aefc6df28772ec3d
tiziole <tizianolettieri@gmail.com>
2025-01-15 16:45:29 +0100

    list1

---
+++ parallaxlista.js
@@ -0,0 +1,35 @@
+function setParallaxEffect() {
+        var cards = document.querySelectorAll('.card');
+        var windowHeight = window.innerHeight;
+        var startTop = 30; // Valore iniziale di top
+        var endTop = -3; // Valore finale di top
+
+        cards.forEach(function(card) {
+            var image = card.querySelector('.card-image');
+            var rect = card.getBoundingClientRect();
+
+            // Se la card è visibile nella viewport
+            if (rect.top <= windowHeight && rect.bottom >= 0) {
+                var distanceFromCenter = windowHeight / 2 - rect.top; // Distanza dalla metà della viewport
+                var parallax = startTop - (distanceFromCenter * 0.15); // Calcola il movimento parallax
+
+                // Assicurati che il valore parallax non superi il range specificato
+                parallax = Math.max(endTop, Math.min(startTop, parallax));
+
+                // Applica il movimento parallax all'immagine
+                image.style.transform = `translate(-50%, ${parallax}px)`;
+            }
+        });
+    }
+
+    function onScroll() {
+        if (!window.requestAnimationFrame) {
+            setParallaxEffect();
+        } else {
+            requestAnimationFrame(setParallaxEffect);
+        }
+    }
+
+    window.addEventListener('scroll', onScroll);
+    window.addEventListener('resize', setParallaxEffect);
+    document.addEventListener("DOMContentLoaded", setParallaxEffect);
