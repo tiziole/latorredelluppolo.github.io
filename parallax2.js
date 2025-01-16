79ff09e890736cf1d204d6a6a4f0b71d308043eb
tiziole <tizianolettieri@gmail.com>
2025-01-16 14:18:16 +0100

    upgrade grid beta1

--- parallax2.js
+++ parallax2.js
@@ -2,7 +2,7 @@ function setParallaxEffect() {
         var cards = document.querySelectorAll('.card');
         var windowHeight = window.innerHeight;
         var startTop = 40; // Valore iniziale di top
-        var endTop = -5; // Valore finale di top
+        var endTop = -15; // Valore finale di top
 
         cards.forEach(function(card) {
             var image = card.querySelector('.card-image');
